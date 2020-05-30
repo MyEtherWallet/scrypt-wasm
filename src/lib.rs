502d68302667df3b3455d383d09e555b810163d3
Simon Warta <simon@warta.it>
2020-05-30 23:04:14 +0200

    Format code

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,31 @@
-extern crate wasm_bindgen;
-extern crate hex;
 extern crate crypto;
+extern crate hex;
+extern crate wasm_bindgen;
 
-use crypto::{ scrypt };
-use wasm_bindgen::prelude::*;
+use crypto::scrypt;
 use std::iter::repeat;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn scrypt(password: &str, salt: &str,  n: u32, r: u32, p: u32, dklen: usize) -> String {
-    let err_str= String::from("input Error");
+pub fn scrypt(password: &str, salt: &str, n: u32, r: u32, p: u32, dklen: usize) -> String {
+    let err_str = String::from("input Error");
     let log_n = (32 - n.leading_zeros() - 1) as u8;
     if log_n as u32 >= r * 16 {
-		return String::from("Invalid r");
-	}
-	if p as u64 > ((u32::max_value() as u64 - 1) * 32)/(128 * (r as u64)) {
-		return String::from("Invalid p");
-	}
+        return String::from("Invalid r");
+    }
+    if p as u64 > ((u32::max_value() as u64 - 1) * 32) / (128 * (r as u64)) {
+        return String::from("Invalid p");
+    }
     let mut result: Vec<u8> = repeat(0).take(dklen).collect();
-     let params = scrypt::ScryptParams::new(log_n,r,p);
-     let pass_ = match hex::decode(password){
-         Ok(p) => p,
-         Err(_err) => return err_str,
-     };
-     let salt_ = match hex::decode(salt){
-         Ok(p) => p,
-         Err(_err) => return err_str,
-     };
-     scrypt::scrypt(&pass_, &salt_, &params, &mut result);
-     hex::encode(result)
+    let params = scrypt::ScryptParams::new(log_n, r, p);
+    let pass_ = match hex::decode(password) {
+        Ok(p) => p,
+        Err(_err) => return err_str,
+    };
+    let salt_ = match hex::decode(salt) {
+        Ok(p) => p,
+        Err(_err) => return err_str,
+    };
+    scrypt::scrypt(&pass_, &salt_, &params, &mut result);
+    hex::encode(result)
 }
